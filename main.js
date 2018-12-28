84e0ee80ff9434b7e164fbfdc3b05551a3399ae5
Jovi <redzjovi@gmail.com>
2018-12-28 15:43:45 +0700

    5. Event Handling

--- main.js
+++ main.js
@@ -8,13 +8,23 @@ var app = new Vue({
     variants: [
       {
         variantId: 2234,
-        variantColor: 'green'
+        variantColor: 'green',
+        variantImage: 'assets/vmSocks-green.jpg'
       },
       {
         variantId: 2235,
-        variantColor: 'blue'
+        variantColor: 'blue',
+        variantImage: 'assets/vmSocks-blue.jpg'
       }
     ],
-    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+    cart: 0
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1
+    },
+    updateProduct: function (variantImage) {
+      this.image = variantImage
+    }
   }
 });
