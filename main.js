2b5c27a39c63c277d1e654a1c56d5f20ba32bfec
Jovi <redzjovi@gmail.com>
2018-12-28 14:53:09 +0700

    2. Attribute Binding  - Challenge

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: 'assets/vmSocks-green.jpg'
+    image: 'assets/vmSocks-green.jpg',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
   }
 });
