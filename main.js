3a4f5c6c87d4037f77b3729ace0c1a19c42ccc74
Jovi <redzjovi@gmail.com>
2019-01-14 10:39:35 +0700

    8. Components - Challange

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `
+})
+
 Vue.component('product', {
   props: {
     premium: {
@@ -16,9 +30,7 @@ Vue.component('product', {
         <p v-else>Out of Stock</p>
         <p>Shipping {{ shipping }}</p>
 
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
+        <product-details :details="details"></product-details>
 
         <div v-for="(variant, index) in variants"
           :key="variant.variantId"
