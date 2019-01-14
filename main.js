8c2a2f763be9e0fce8dfec008b1e5eae56910a82
Jovi <redzjovi@gmail.com>
2019-01-14 16:04:17 +0700

    11. Tabs - Challenge

--- main.js
+++ main.js
@@ -16,11 +16,8 @@ Vue.component('product', {
         <h1>{{ title }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
-        <p>Shipping {{ shipping }}</p>
 
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
+        <info-tabs :shipping="shipping" :details="details"></info-tabs>
 
         <div v-for="(variant, index) in variants"
           :key="variant.variantId"
@@ -195,6 +192,48 @@ Vue.component('product-tabs', {
     return {
       tabs: ['Reviews', 'Make a Review'],
       selectedTab: 'Reviews'
+    }
+  }
+});
+
+Vue.component('info-tabs', {
+  props: {
+    shipping: {
+      required: true
+    },
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <div>
+
+      <ul>
+        <span class="tabs"
+          :class="{ activeTab: selectedTab === tab }"
+          v-for="(tab, index) in tabs"
+          @click="selectedTab = tab"
+          :key="tab"
+        >{{ tab }}</span>
+      </ul>
+
+      <div v-show="selectedTab === 'Shipping'">
+        <p>{{ shipping }}</p>
+      </div>
+
+      <div v-show="selectedTab === 'Details'">
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+      </div>
+
+    </div>
+  `,
+  data() {
+    return {
+      tabs: ['Shipping', 'Details'],
+      selectedTab: 'Shipping'
     }
   }
 });
